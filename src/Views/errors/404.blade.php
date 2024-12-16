f0079fbd794677b384e9e3f75b1c5cb43ffce2a4
wang9 <wtc>
2024-12-16 17:57:41 +0800

    配置

--- src/Views/errors/404.blade.php
+++ src/Views/errors/404.blade.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <link rel="stylesheet" href="{{customAsset('asset/layuiadmin/layui/css/layui.css')}}" media="all">
-    <link rel="stylesheet" href="{{customAsset('asset/layuiadmin/style/admin.css')}}" media="all">
-    <link rel="stylesheet" href="{{customAsset('asset/base/base.css')}}">
+    <link rel="stylesheet" href="{{customAsset('assets/layuiadmin/layui/css/layui.css')}}" media="all">
+    <link rel="stylesheet" href="{{customAsset('assets/layuiadmin/style/admin.css')}}" media="all">
+    <link rel="stylesheet" href="{{customAsset('assets/base/base.css')}}">
 </head>
 <body>
 
