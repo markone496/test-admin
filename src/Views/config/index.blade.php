0fe00f8a5f8cfaf3c7068761c91bfb3ca6246460
wang9 <wtc>
2024-12-16 16:57:09 +0800

    基础框架

--- src/Views/config/index.blade.php
+++ src/Views/config/index.blade.php
@@ -1,49 +1,40 @@
 @extends('lzadmin.layouts.app')
-@section('title', '')
+@section('title', '编辑页')
 @section('styles')
+    <style>
+        html {
+            background: #FFF;
+        }
+    </style>
 @endsection
 
 @section('content')
-    <div class="layui-fluid">
-        <div class="layui-row layui-col-space15">
-            <div class="layui-col-md6">
-                <div class="layui-card">
-                    <div class="layui-card-header">{{$model['title']}}【{{$model['index_key']}}】</div>
-                    <div class="layui-card-body">
-                        <form class="layui-form">
-                            @foreach($form as $html)
-                                {!! $html !!}
-                            @endforeach
-                            @if($auth)
-                                <div class="layui-form-item">
-                                    <div class="layui-input-block">
-                                        <button class="layui-btn" lay-submit lay-filter="submit">确认保存</button>
-                                    </div>
-                                </div>
-                            @endif
-                        </form>
-                    </div>
-                </div>
-            </div>
+    <form class="layui-form open-content">
+        <input type="hidden" name="id" value="{{$model->id}}">
+        @foreach($editForm as $html)
+            {!! $html !!}
+        @endforeach
+        <div class="layui-form-item layui-hide">
+            <input type="button" lay-submit lay-filter="submit" id="submit">
         </div>
-    </div>
+    </form>
 @endsection
 @section('scripts')
     <script>
+        var call = null;
         layui.use(function () {
             var form = layui.form;
+
             // 表单提交
             form.on('submit(submit)', function (data) {
-                let field = data.field;
-                com.post("/sys/config/update/{{$model['index_key']}}", field, function (res) {
-                    if (res.code) {
-                        layer.msg(res.msg, {icon: 2})
-                    } else {
-                        layer.msg(res.msg, {icon: 1})
-                    }
-                });
+                call && call(data.field);
                 return false;
             });
-        })
+        });
+
+        function submit(func) {
+            call = func;
+            $('#submit').click();
+        }
     </script>
 @endsection
